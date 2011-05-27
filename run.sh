d12f5991b78ac993d66a923a4cbe83182ef59ce1
disq <kemal@hadimli.com>
2011-05-27 18:57:12 +0300

    added usage help & countdown from 5 on miner quit

--- run.sh
+++ run.sh
@@ -11,8 +11,23 @@ for i in $*; do
 		exit 1
 	fi
 done
+if [ "$MINER" == "" ]; then
+    echo \$MINER not set, exiting
+    echo Usage: $0 one.device one.pool one.miner
+    exit 1
+fi
 while true; do
 	echo $MINER
 	eval $MINER
-	sleep 3
+	echo -n miner quit, restarting in 5
+	sleep 1
+	echo -n " 4"
+	sleep 1
+	echo -n " 3"
+	sleep 1
+	echo -n " 2"
+	sleep 1
+	echo -n " 1"
+	sleep 1
+	echo " now"
 done
