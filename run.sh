606d9048c204ea95e8da1691d29f9a87a8d0e487
disq <kemal@hadimli.com>
2011-06-17 16:11:26 +0300

    detect symlinks & show linked filenames

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+function resolve {
+local p=$1
+local f=`readlink -f $p`
+local r=`basename $f`
+if [ "$p" == "$r" ]; then
+    echo "Reading $p"
+else
+    echo "Reading $p ($r)"
+fi
+}
+
 declare -a PARAMS
 PMODE=0
 for i in $*; do
@@ -9,10 +21,10 @@ for i in $*; do
 		if [ "$i" == "-P" ]; then
 			PMODE=1
 		elif [ -e $i ]; then
-			echo "Reading $i"
+			resolve $i
 			source $i
 		elif [ -e "./$i" ]; then
-			echo "Reading ./$i"
+			resolve "./$i"
 			source ./$i
 		else
 			echo "Not understood: $i"
@@ -20,6 +32,7 @@ for i in $*; do
 		fi
 	fi
 done
+
 if [ "$MINER" == "" ]; then
     echo \$MINER not set, exiting
     echo Usage: $0 one.device one.pool one.miner [extra file] [-P extra params [extra params [ ... ]]]
